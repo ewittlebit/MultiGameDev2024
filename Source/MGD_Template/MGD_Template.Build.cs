0e689f84a0f3cddb1877737e3e21eec2db8c2d0e
ewittlebit <126030014+ewittlebit@users.noreply.github.com>
2024-09-26 16:22:18 +1000

    working

--- Source/MGD_Template/MGD_Template.Build.cs
+++ Source/MGD_Template/MGD_Template.Build.cs
@@ -8,7 +8,7 @@ public class MGD_Template : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AnimationData" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
